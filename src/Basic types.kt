b8a72e53c1f56b8b748d3cbcea975c7a93879223
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-19 08:17:29 +0200

    basic type

---
+++ src/Basic types.kt
@@ -0,0 +1,19 @@
+package src
+
+fun main() {
+    // Explicitly declare the correct type for each variable:
+
+//    val a = 1000
+//    val b = "log message"
+//    val c = 3.14
+//    val d = 100_000_000_000_000
+//    val e = false
+//    val f = '\n'
+
+    val a: Int =1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
