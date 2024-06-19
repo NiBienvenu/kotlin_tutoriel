4d8bbad96bcb3412c7ad260c9ef2847202c82d43
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-19 08:10:07 +0200

    varables

---
+++ src/Variables﻿.kt
@@ -0,0 +1,16 @@
+package src
+
+fun main() {
+
+    /*
+    * Complete the code to make the program print
+    * "Mary is 20 years old" to standard output:
+   * */
+    val constomer = 0
+    val name = "Mary"
+    val age = 20
+    println("$name is $age old to standard")
+
+    var i = 0 // mutable variable
+    val j = 0 // read only
+}
