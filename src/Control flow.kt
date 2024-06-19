8440b536899683ee4547fed7b744a182a80d8539
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-19 11:42:50 +0200

    Control flow

--- src/Control flow.kt
+++ src/Control flow.kt
@@ -9,31 +9,76 @@ fun main() {
 
     // image file /src/control flow image
 
-    val button = "B"
+    val button = "E"
 
     println(
-
             when (button) {
                 "A" -> {
-                    println("YES")
+                    "YES"
                 }
                 "B" -> {
-                    println("NON")
+                    "NON"
                 }
                 "X" -> {
-                    println("Menu")
+                    "Menu"
                 }
                 "Y" -> {
-                    println("Nothing")
+                    "Nothing"
                 }
                 "Other" ->{
-                    println("There is no such button")
+                    "There is no such button"
                 }
                 else -> {
                     println("Unknown")
                 }
 
         }
-
     )
+
+    /*
+    * Vous avez un programme qui compte les tranches de pizza jusqu'à ce qu'il y ait une pizza entière avec 8 tranches.
+    * Refactorisez ce programme de deux manières
+    * */
+    var pizzaSlices = 0
+    // Start refactoring here
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+    // End refactoring here
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+
+    println("======================== 2 WAYS =================")
+
+    for (i in 1..8){
+        println("There's only $i slice/s of pizza :(")
+    }
+    while (pizzaSlices<8){
+        println("There's only $pizzaSlices slice/s of pizza :(")
+        pizzaSlices++
+    }
+
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+
+    println("======================= 3 WAYS =================")
+
+    do {
+        println("There's only $pizzaSlices slice/s of pizza :(")
+        pizzaSlices++
+    } while ( pizzaSlices < 8 )
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+
+
+
 }
