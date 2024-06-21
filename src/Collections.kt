006573aa3b273bb439559f104013ef5a862ece38
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-21 23:44:43 +0200

    MutableList set

--- src/Collections.kt
+++ src/Collections.kt
@@ -66,6 +66,30 @@ fun main() {
         }
         // getSysSudoers().add(5) <- Error!                           // 8
 
+    /*
+    * 1. Creates a Set with given elements.
+      2. Returns a boolean value showing if the element was actually added.
+      3. Returns a string, based on function input parameter.
+      4. Prints a success message: the new element is added to the Set.
+      5. Prints a failure message: the element can't be added because it duplicates an existing element.
+    * */
 
 
+
+        val aNewIssue: String = "uniqueDescr4"
+        val anIssueAlreadyIn: String = "uniqueDescr2"
+
+        println("Issue $aNewIssue ${getStatusLog(addIssue(aNewIssue))}")                              // 4
+        println("Issue $anIssueAlreadyIn ${getStatusLog(addIssue(anIssueAlreadyIn))}")                // 5
+
+
+}
+val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3") // 1
+
+fun addIssue(uniqueDesc: String): Boolean {
+    return openIssues.add(uniqueDesc)                                                             // 2
+}
+
+fun getStatusLog(isAdded: Boolean): String {
+    return if (isAdded) "registered correctly." else "marked as duplicate and rejected."          // 3
 }
