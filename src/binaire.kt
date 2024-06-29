db73d4f7d4ab7008a594330ae93f5fe18f694248
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-29 06:01:22 +0200

    BINAIRE

---
+++ src/binaire.kt
@@ -0,0 +1,14 @@
+package src
+
+
+fun main() {
+    println(0x10) //16
+    println(0x000000) //BLACK
+    println(0xFFFFFF) //WHITE
+    println(0xFF0000) //RED
+    println(0x00FF00) //GREEN
+    println(0x0000FF) //BLUE
+    println(0x000088) //DARK BLUE
+
+    println(0x000000 or 0xFFFFFF) //
+}
