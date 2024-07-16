19c878ed8d4db693e1e285109b70c680a5a0fd28
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-16 18:07:19 +0200

    declaration

---
+++ src/algorithime/algorithime.kt
@@ -0,0 +1,15 @@
+package src.algorithime
+
+
+fun main() {
+    print("Taper le mois :")
+    val mois = readln()
+    print("Taper l'annee :")
+    val annee = readln()
+
+    try {
+
+    }catch (e: Exception) {
+        println(e.message)
+    }
+}
